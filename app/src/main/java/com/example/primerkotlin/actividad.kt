e39f1e95acdb3ba3adc4557f2e704d6bb61afe95
katthy <you@example.com>
2020-08-28 22:58:25 -0400

    metodos y clases metodos

--- app/src/main/java/com/example/primerkotlin/actividad.kt
+++ app/src/main/java/com/example/primerkotlin/actividad.kt
@@ -7,8 +7,16 @@ fun main(){
     val mCustomer = Customer()
     val mContact = Contact(1,"katherinaseguel@gmail.com")
 
+
     println(mContact.id)
     println(mContact.email)
+
+
+    val perrito = Dog() //se instancio un objeto
+    perrito.makeNoise()
+    val candy:Dog= candy()
+    candy.makeNoise()
+
 }
 
 class FirstClass  //pueder ser sin variebles
@@ -16,9 +24,24 @@ class FirstClass  //pueder ser sin variebles
 class Persona(text:String){
 
 }
+
+
 ///HAY QUE TENER 1 archivo por CLASE
 class Customer //clase sin parámetos
 
 class Contact (val id:Int, var email:String) //si no llev nada más no se necesitan las llves
 
+//clase para heredar debe llevar }OPEn para heredar igual que los métodos
 
+open class Dog{
+  open  fun makeNoise(){           //METODO
+        println("woau wuau")
+    }
+}
+class candy:Dog(){            //herencia
+    override fun makeNoise() {
+        //super.makeNoise()   //cambio lo que hace el metodo, lo puedo comentar o no
+        println("wuf wuf")  // lo dejo así
+
+    }
+}
